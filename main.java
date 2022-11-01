73b1081aa5f58adc1a82c938aacc831cb10a095a
Dilara Valitova <87713511+valitovaa@users.noreply.github.com>
2022-11-01 13:32:36 +0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Main {
+    public static void main(String[] args) {
+        SimpleGUI myFrame = new SimpleGUI(640,480);
+        myFrame.setUpGUI();
+
+    }
+}
